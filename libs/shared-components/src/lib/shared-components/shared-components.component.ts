a4186242dc9c2786872869cddf64fe306b983969
Jean Boittiaux <jean.boittiaux@gmail.com>
2024-09-30 14:11:25 +0200

    Mise en place d'un formulaire de login partagé

--- libs/shared-components/src/lib/shared-components/shared-components.component.ts
+++ libs/shared-components/src/lib/shared-components/shared-components.component.ts
@@ -1,11 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'lib-shared-components',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './shared-components.component.html',
   styleUrl: './shared-components.component.scss',
 })
-export class SharedComponentsComponent {}
+export class SharedComponentsComponent {
+  private fb = inject(FormBuilder);
+
+  form = this.fb.group({
+    login: this.fb.control('', [
+      Validators.required,
+    ]),
+    password: this.fb.control('', [
+      Validators.required,
+    ]),
+  });
+
+  get isLogged() {
+    const logged = localStorage.getItem('logged');
+    return logged ? JSON.parse(logged) : null;
+  }
+
+  onSubmit() {
+    if (this.form.valid) {
+      localStorage.setItem('logged', JSON.stringify(this.form.value));
+    }
+  }
+
+  logout() {
+    localStorage.removeItem('logged');
+  }
+}
